a0404cc1c0389d2e528556285c1c6a8f1badaae3
kiphix <148585270+jdaddar@users.noreply.github.com>
2024-10-28 21:26:17 -0400

    'undo' functionality

--- canvas.py
+++ canvas.py
@@ -1,32 +1,47 @@
 from PySide6 import QtWidgets, QtGui
 from PySide6.QtCore import Qt
+from collections import deque
 
 class Canvas(QtWidgets.QLabel):
-    def __init__(self, w: int, h: int):
+    def __init__(self, w: int, h: int): #TODO: make init more functional
         super().__init__()
 
-        # Create and set pixmap for canvas, default black
-        canvas_pixmap = QtGui.QPixmap(w, h)
-        canvas_pixmap.fill(QtGui.QColor('black')) # TODO: load from setting
-        self.setPixmap(canvas_pixmap)
+        # Settings
+        self.canvas_color = 'black' # TODO: load from setting
+        max_undo = 500 # rec values: low:20, mid:50, high:200, ultra:500
+                       # max mb ram:    100,    215       770
 
+        # Create and set pixmap for canvas, using default color
+        self.initial_pixmap = QtGui.QPixmap(w, h)
+        self.initial_pixmap.fill(QtGui.QColor(self.canvas_color))
+        self.setPixmap(self.initial_pixmap)
+
+        # Initializing useful variables 
         self.prev_x, self.prev_y = None, None
-        self.brush_size = 3
+        self.pixmap_stack = deque([], max_undo)
+
+        # Brush settings
+        self.brush_size = 4
         self.brush_primary_color = QtGui.QColor('white') # TODO: load from setting
         self.brush_secondary_color = QtGui.QColor('black') # TODO: load from setting
 
     def set_brush_size(self, size):
+        """ Set brush size """
         self.brush_size = size;
     
     def set_brush_primary_color(self, color):
+        """ Set brush primary color """
         self.brush_primary_color = QtGui.QColor(color)
 
     def set_brush_secondary_color(self, color):
+        """ Set brush secondary color """
         self.brush_secondary_color = QtGui.QColor(color)
 
     def draw_brush_point(self, x, y, color):
-        """ Paint point with brush at pos(x, y) using brush of specified color """
-        current_pixmap = self.pixmap() # TODO: save previous canvases so user can 'undo'
+        """ 
+        Paint point with brush at pos(x, y) using brush of specified color 
+        """
+        current_pixmap = self.pixmap()
         painter = QtGui.QPainter(current_pixmap)
         # TODO: test if brush is more sensible
         pen = painter.pen()
@@ -42,7 +57,7 @@ class Canvas(QtWidgets.QLabel):
         Paint line with brush from pos(start_x, start_y) to pos(x, y) 
         of specified color
         """
-        current_pixmap = self.pixmap() # TODO: save previous canvases so user can 'undo'
+        current_pixmap = self.pixmap()
         painter = QtGui.QPainter(current_pixmap)
         pen = painter.pen()
         pen.setWidth(self.brush_size)
@@ -52,33 +67,62 @@ class Canvas(QtWidgets.QLabel):
         painter.end()
         self.setPixmap(current_pixmap)
 
+    def saveCanvas(self, pixmap):
+        """ Add pixmap to 'undo' stack """
+        self.pixmap_stack.append(pixmap)
+
     def mousePressEvent(self, e):
         # Set mouse position start for movement tracking
-        self.prev_x = e.position().x()
-        self.prev_y = e.position().y()
+        self.prev_x = e.position().toPoint().x()
+        self.prev_y = e.position().toPoint().y()
 
         # Paint point of primary/secondary color based on left/right click
         if e.buttons() == Qt.LeftButton:
-            self.draw_brush_point(e.position().x(), e.position().y(), self.brush_primary_color)
+            self.saveCanvas(self.pixmap())
+            self.draw_brush_point(e.position().toPoint().x(), 
+                                  e.position().toPoint().y(), 
+                                  self.brush_primary_color)
         elif e.buttons() == Qt.RightButton:
-            self.draw_brush_point(e.position().x(), e.position().y(), self.brush_secondary_color)
+            self.saveCanvas(self.pixmap())
+            self.draw_brush_point(e.position().toPoint().x(), 
+                                  e.position().toPoint().y(), 
+                                  self.brush_secondary_color)
 
     def mouseMoveEvent(self, e):
         if self.prev_x is None: # First event
-            self.prev_x = e.position().x()
-            self.prev_y = e.position().y()
+            self.prev_x = e.position().toPoint().x()
+            self.prev_y = e.position().toPoint().y()
             return # Ignore first time, already drawing point
 
-        # Paint line of primary/secondary color based on left/right click from previous mouse pos to current pos 
+        # Paint line of primary/secondary color based on left/right click 
+        # from previous mouse pos to current pos 
         if e.buttons() == Qt.LeftButton:
-            self.draw_brush_line(self.prev_x, self.prev_y, e.position().x(), e.position().y(), self.brush_primary_color)
+            self.draw_brush_line(self.prev_x, 
+                                 self.prev_y, 
+                                 e.position().toPoint().x(), 
+                                 e.position().toPoint().y(), 
+                                 self.brush_primary_color)
         elif e.buttons() == Qt.RightButton:
-            self.draw_brush_line(self.prev_x, self.prev_y, e.position().x(), e.position().y(), self.brush_secondary_color)
+            self.draw_brush_line(self.prev_x, 
+                                 self.prev_y, 
+                                 e.position().toPoint().x(), 
+                                 e.position().toPoint().y(), 
+                                 self.brush_secondary_color)
 
         # Update mouse loc
-        self.prev_x = e.position().x()
-        self.prev_y = e.position().y()
+        self.prev_x = e.position().toPoint().x()
+        self.prev_y = e.position().toPoint().y()
             
     def mouseReleaseEvent(self, e):
-        """ Reset previous position on mouse release """
+        # Reset previous positions
         self.prev_x, self.prev_y = None, None
+
+    def undo(self):
+        """ 
+        Reverts to previous pixmap, effectively undoing 
+        most recent draw action 
+        """
+        try:
+            self.setPixmap(self.pixmap_stack.pop())
+        except IndexError: # IndexError can only occur if stack is empty,
+            pass           # therefore pass as we have reached undo limit 
--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from PySide6.QtCore import Qt
 from canvas import Canvas
 
 class NightPainterWindow(QtWidgets.QMainWindow):
-    def __init__(self):
+    def __init__(self): # TODO: make init more functional
         super().__init__()
-        
+
         self.setWindowTitle("Night Painter")
 
         # TODO: fix all this to use settings to maintain previous sizes
@@ -17,8 +17,10 @@ class NightPainterWindow(QtWidgets.QMainWindow):
         self.setScreen(self.primaryScreen)
         # Set initial window size based on screen size
         initial_size_divider = 2
-        initial_width = self.primaryScreen.availableSize().width()//initial_size_divider
-        initial_height = self.primaryScreen.availableSize().height()//initial_size_divider
+        initial_width = (self.primaryScreen.availableSize().width()
+                         //initial_size_divider)
+        initial_height = (self.primaryScreen.availableSize().height()
+                          //initial_size_divider)
         self.resize(initial_width, initial_height)
         self.setMinimumSize(320, 180) # Avoid making window too small for use
 
@@ -28,6 +30,13 @@ class NightPainterWindow(QtWidgets.QMainWindow):
 
         self.setCentralWidget(self.canvas)
 
+    def keyPressEvent(self, e:QtGui.QKeyEvent):
+        # 'Undo' hotkey
+        undo_hotkey = Qt.KeyboardModifier.ControlModifier|Qt.Key.Key_Z
+        if e.keyCombination() == undo_hotkey:
+            self.canvas.undo()
+
+# Run app
 app = QtWidgets.QApplication([])
 window = NightPainterWindow()
 window.show()
